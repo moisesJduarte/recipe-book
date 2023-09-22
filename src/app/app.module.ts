aeed85b94c74b268e5b04e220862252211b677ed
Moises Duarte <gladmd19@gmail.com>
2023-09-21 20:32:47 -0400

    adding  components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { RecipesListComponent } from './components/recipes/recipes-list/recipes-list.component';
+import { RecipesDetailComponent } from './components/recipes/recipes-detail/recipes-detail.component';
+import { RecipesItemComponent } from './components/recipes/recipes-list/recipes-item/recipes-item.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HeaderComponent,
     FooterComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesDetailComponent,
+    RecipesItemComponent,
   ],
   imports: [
     BrowserModule,
