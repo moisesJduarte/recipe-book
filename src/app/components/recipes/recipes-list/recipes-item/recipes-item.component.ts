c92d6a2fb96ec6028c92298ad2d00942d6392dc2
Moises Duarte <gladmd19@gmail.com>
2023-10-02 23:00:31 -0400

    binding components

--- src/app/components/recipes/recipes-list/recipes-item/recipes-item.component.ts
+++ src/app/components/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Recipes } from '../../recipes.model';
 @Component({
   selector: 'app-recipes-item',
   templateUrl: './recipes-item.component.html',
   styleUrls: ['./recipes-item.component.css']
 })
 export class RecipesItemComponent {
-
+  @Input() recipes: Recipes;
 }
